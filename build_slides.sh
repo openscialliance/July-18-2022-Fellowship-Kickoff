8d16810806a6be6a3b1db36ed38ad3596aa68d0d
Shannon Quinn <magsol@gmail.com>
2022-07-18 09:38:03 -0400

    Updated slides.

--- build_slides.sh
+++ build_slides.sh
@@ -3,29 +3,24 @@
 # Borrowed somewhat from https://medium.com/learning-machine-learning/present-your-data-science-projects-with-jupyter-slides-75f20735eb0f
 
 PREFIX=IntroExpectations
-PTH=slides
+BUILD=slides
+RENDER=docs
 
 # Download reveal.js
 #wget https://github.com/hakimel/reveal.js/archive/master.zip
 #unzip master.zip
 
-
 # Available reveal themes: https://github.com/hakimel/reveal.js/tree/master/css/theme/source
 
-# Convert ipynb to html
-#jupyter nbconvert ${PTH}/${PREFIX}.ipynb --to slides --post serve \
-#	--SlidesExporter.reveal_theme=solarized \
-#	--SlidesExporter.reveal_scroll=True \
-#	--SlidesExporter.reveal_transition=fade
-jupyter nbconvert ${PTH}/${PREFIX}.ipynb --to slides \
+jupyter nbconvert ${BUILD}/${PREFIX}.ipynb --to slides \
 	--SlidesExporter.reveal_theme=solarized \
 	--SlidesExporter.reveal_scroll=True \
 	--SlidesExporter.reveal_transition=fade
-mv ${PTH}/${PREFIX}.html ${PTH}/${PREFIX}.slides.html
+mv ${BUILD}/${PREFIX}.slides.html ${RENDER}/index.html
 
 # Convert ipynb to pdf
-jupyter nbconvert ${PTH}/${PREFIX}.ipynb --to latex
-mv ${PTH}/${PREFIX}.tex .
+jupyter nbconvert ${BUILD}/${PREFIX}.ipynb --to latex
+mv ${BUILD}/${PREFIX}.tex .
 pdflatex ${PREFIX}.tex
 rm ${PREFIX}.aux ${PREFIX}.log ${PREFIX}.out ${PREFIX}.tex
-mv ${PREFIX}.pdf ${PTH}/${PREFIX}.slides.pdf
+mv ${PREFIX}.pdf ${RENDER}/slides.pdf
